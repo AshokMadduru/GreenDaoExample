e26f1a8350fe7494778e79479855ebfc4b1f9fc9
ashokm <ashok.madduru@riktamtech.com>
2016-12-30 15:40:08 +0530

    Add application class. Add greendao initialize classes

---
+++ app/src/main/java/in/shoksworld/greendao/GreenDao.java
@@ -0,0 +1,42 @@
+package in.shoksworld.greendao;
+
+import android.app.Application;
+import android.database.sqlite.SQLiteDatabase;
+
+import in.shoksworld.greendao.storage.dbgenerator.DaoMaster;
+import in.shoksworld.greendao.storage.dbgenerator.DaoSession;
+
+/**
+ * Application Class
+ */
+
+public class GreenDao extends Application {
+    static GreenDao applicationInstance;
+    public DaoSession daoSession;
+    public String databaseName = "greendaodemo-database";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        applicationInstance = this;
+    }
+
+    /**
+     * @return application instance
+     */
+    public static synchronized GreenDao getInstance() {
+        return applicationInstance;
+    }
+
+    public DaoSession getDaoSession() {
+        if (this.daoSession == null) {
+            DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(GreenDao.this,
+                    databaseName, null);
+            SQLiteDatabase database = helper.getWritableDatabase();
+            DaoMaster daoMaster = new DaoMaster(database);
+            this.daoSession = daoMaster.newSession();
+        }
+        return daoSession;
+    }
+}
